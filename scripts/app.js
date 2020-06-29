ff47e1faaa323973dea3d76f526257d8328d1d73
Liam McAllister <liammca1987@gmail.com>
2020-06-29 16:03:36 +0100

    Fixed contact.html layout

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
 
-    //Quick Book Form Slide
-    $('#quick-book__header').click(function () {
-        $('#quick-book__form').slideToggle('slow');
+  //Quick Book Form Slide
+  let rotateDeg = 0;
+  $("#quick-book__header").click(function () {
+    $("#quick-book__form").slideToggle("slow");
+    rotateDeg += 180;
+    $("#quick-book-arrow").css({
+      transform: "rotate(" + rotateDeg + "deg)",
+      "-ms-transform": "rotate(" + rotateDeg + "deg)",
+      "-moz-transform": "rotate(" + rotateDeg + "deg)",
+      "-webkit-transform": "rotate(" + rotateDeg + "deg)",
+      "-o-transform": "rotate(" + rotateDeg + "deg)",
     });
+  });
 
+  
 });
