9860b4f1434f2a21bfd57fe7f75ef27a2ea6eb10
liam-mcallister <liammca1987@gmail.com>
2021-03-08 17:03:31 +0000

    Added video to index hero

---
+++ src/scripts/validation.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    // Quick Book Form Validation (Validate Plugin)
+    $('#quick-form').validate({
+        errorPlacement: function (error, element) {
+            error.insertBefore(element);
+        },
+        rules: {
+            quickName: {
+                required: true,
+                minlength: 3,
+                maxlength: 30
+            },
+            quickPhone: {
+                required: true,
+                digits: true
+            },
+            quickDate: {
+                required: true
+            },
+            quickTime: {
+                required: true
+            },
+            quickPeople: {
+                required: true
+            }
+        }
+    });
+
+});
